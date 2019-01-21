f92f4f239a2c0e4e09b588be62d811cfcfa904a0
Kimmy <kenpusney@gmail.com>
2019-01-21 13:28:04 +0800

    feat(sdk-example): initialize

---
+++ src/main/java/com/baozun/casaba/example/App.java
@@ -0,0 +1,16 @@
+package com.baozun.casaba.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+@ComponentScan
+public class App {
+
+  public static void main(String[] args) {
+    SpringApplication.run(App.class);
+  }
+}
---
+++ src/test/java/com/baozun/casaba/example/AppTest.java
@@ -0,0 +1,16 @@
+package com.baozun.casaba.example;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class AppTest {
+
+  @Test
+  public void contextLoads() {
+
+  }
+}
